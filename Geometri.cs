5b5a2eebcd4496411fc953ba051fab6b540395b4
Ludwig Svensson <ludwig.gifrod@hotmail.com>
2023-10-30 13:32:32 +0100

    Add project files.

---
+++ Geometri.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3___Polymorphsim
+{
+    public class Geometry
+    {
+
+        public virtual double Area()
+        {
+            double areaValue = 0;
+            return areaValue;
+        }
+    }
+    public class Rectangle : Geometry
+    {
+        public double _length { get; set; }
+        public double _width { get; set; }
+        public Rectangle(double length, double width)
+        {
+            _length = length;
+            _width = width;
+        }
+        public override double Area()
+        {
+            return _length * _width;
+        }
+    }
+    public class Circle : Geometry
+    {
+        public double _radius { get; set; }
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+        public override double Area()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+    public class Square : Geometry
+    {
+        public double _side { get; set; }
+        public Square (double side)
+        {
+            _side = side;
+        }
+        public override double Area()
+        {
+            return _side* _side;
+        }
+    }
+    public class Triangle : Geometry
+    {
+        public double _sideA { get; set; }
+        public double _sideB { get; set; }
+        public double _sideC { get; set; }
+        public Triangle(double sideA,double sideB, double sideC)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
+        }
+        public override double Area()
+        {
+            return (_sideA + _sideB + _sideC) / 2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Labb_3___Polymorphsim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(7.42);            
+            Square square = new Square(6.30);            
+            Rectangle rectangle = new Rectangle(5.5,9.3);            
+            Triangle triangle = new Triangle(2.4,10,9.4);
+            Console.WriteLine($"Area of the circle is: {circle.Area()}" +
+                $"\nArea of the square is: {square.Area()}" +
+                $"\nArea of the rectangle is: {rectangle.Area()}" +
+                $"\nArea of the rectangle is: {triangle.Area()}");
+
+        }
+    }
+}
